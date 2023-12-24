9ac990de4222cb815f67c60cbb821bdd035b5234
Keva <Keva@DESKTOP-8LDJHCT>
2023-12-24 17:43:53 -0500

    Carga Inicial

---
+++ src/exploradorphishing/ExploradorPhishing.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exploradorphishing;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Keva
+ */
+public class ExploradorPhishing {
+   private static final Map<String, Integer> PalabraClave = new HashMap<>();
+    static {
+        PalabraClave.put("banco", 3);
+        PalabraClave.put("tarjeta de crédito", 3);
+        PalabraClave.put("número de cuenta", 3);
+        PalabraClave.put("número de seguridad social", 3);
+        PalabraClave.put("contraseña", 3);
+        PalabraClave.put("cuenta de correo electrónico", 3);
+        PalabraClave.put("actualización de software", 2);
+        PalabraClave.put("factura pendiente", 2);
+        PalabraClave.put("ganador del premio", 2);
+        PalabraClave.put("urgente", 2);
+        PalabraClave.put("importante", 2);
+        PalabraClave.put("cerrar su cuenta", 2);
+        PalabraClave.put("su cuenta ha sido bloqueada", 2);
+        PalabraClave.put("su cuenta ha sido suspendida", 2);
+        PalabraClave.put("su cuenta ha sido comprometida", 2);
+        PalabraClave.put("su cuenta está en riesgo", 2);
+        PalabraClave.put("su cuenta ha sido hackeada", 2);
+        PalabraClave.put("su cuenta ha sido pirateada", 2);
+        PalabraClave.put("su cuenta ha sido robada", 2);
+        PalabraClave.put("su cuenta ha sido vulnerada", 2);
+        PalabraClave.put("su cuenta ha sido comprometida", 2);
+        PalabraClave.put("su cuenta ha sido comprometida", 2);
+        PalabraClave.put("su cuenta ha sido comprometida", 2);
+        PalabraClave.put("su cuenta ha sido comprometida", 2);
+    }
+    public static void main(String[] args) throws IOException {
+       // Ruta del archivo de texto a analizar
+        String filePath = "src/exploradorphishing/MiArchivo.txt";
+
+        // Iniciando el contador de puntos
+        int totalPoints = 0;
+
+        // Leer el archivo de texto
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            StringBuilder text = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+
+            // Iteraciones sobre el texto en busca de palabras clave
+            for (String keyword : PalabraClave.keySet()) {
+                int occurrences = text.toString().split(keyword).length - 1;
+                totalPoints += PalabraClave.get(keyword) * occurrences;
+                System.out.println(keyword + " - " + occurrences + " - " + PalabraClave.get(keyword) * occurrences);
+            }
+        }
+
+        // Impresión del total de puntos
+        System.out.println("Total de Puntos: " + totalPoints); 
+        
+    
+    }
+    
+}
